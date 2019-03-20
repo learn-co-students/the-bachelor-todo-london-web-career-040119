168f556291847a2b39bda81ab7b1219090dd6fa9
Francisco Costa <francisco@fcosta.pt>
2019-03-20 14:29:24 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,33 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+
+  winner_first_name = ""
+
+  data[season].each do |season, data|
+    if season["status"] == "Winner"
+       winner_first_name = (season["name"]).split(" ")[0]
+    end
+  end
+
+  winner_first_name
+
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+
+  contestant_name = ""
+
+  data.each do |season, contestants|
+    contestants.each do |contestant, data|
+      if contestant["occupation"] == occupation
+        contestant_name = contestant["name"]
+      end
+    end
+  end
+
+  contestant_name
+
 end
 
 def count_contestants_by_hometown(data, hometown)
