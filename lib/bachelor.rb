21d74a4e38a494def20b29fd1b1526f83dac67ae
Francisco Costa <francisco@fcosta.pt>
2019-03-20 14:58:20 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -47,7 +47,13 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant, data|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
